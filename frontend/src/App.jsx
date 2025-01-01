d41b4dac29814b972306e5ec61fa531ce59f83cf
kh-abrar <kh.abrarr@gmail.com>
2025-01-01 18:57:53 +0600

    Frontend and Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+    {/* Background gradient */}
+    <div className="absolute inset-0 overflow-hidden">
+      <div className="absolute inset-0">
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(0,0,0,0.1)_0%,rgba(128,128,128,0.05)_50%,rgba(255,255,255,0.05)_100%)]" />
+      </div>
+    </div>
+
+    <div className="relative z-50 pt-20">   
+      <Navbar/>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/signup" element={<SignUpPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
+    </div>
+    </div>
+  )
+}
+
+export default App
