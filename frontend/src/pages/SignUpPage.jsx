b7b2524238faef66f589b5b398218a4364c94948
kh-abrar <kh.abrarr@gmail.com>
2025-01-04 19:57:36 +0600

    Auth implementation

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,18 +2,22 @@ import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {UserPlus, Mail, Lock, User, ArrowRight, Loader} from 'lucide-react'
 import {motion} from "framer-motion"
+import { useUserStore } from '../stores/useUserStore'
 
 const SignUpPage = () => {
-  const loading = false;
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     confirmPassword: ""
   });
+
+
+  const {signup, loading} = useUserStore();
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    console.log('Called here');
+    signup(formData);
   }
   
   return (
